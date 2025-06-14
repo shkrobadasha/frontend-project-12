7ac74d39611df0c13d4f91ec11353196afe0c4be
Shkroba Daria <shkrobadaria@mail.ru>
2025-06-14 15:15:05 +0300

    ref 1

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,6 +1,6 @@
 export default {
     translation: {
-        navbarTitle: "My Chat",
+        navbarTitle: 'Hexlet Chat',
         userName: "Ваш ник",
         password: "Пароль",
         exitButton: "Выйти",
