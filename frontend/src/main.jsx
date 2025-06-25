3d263952585fb940d806da7f152e4cfc639f1fbb
Shkroba Daria <shkrobadaria@mail.ru>
2025-06-25 22:02:19 +0300

    check 4-1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 import store from './slices/index.js'
-import './styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 // eslint-disable-next-line no-unused-vars
 import i18next from './18n.js'
 import Socket from './components/Socket.jsx'
