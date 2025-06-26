7c90f4628dce8cca4caf02a3098f4522f204c2fb
Shkroba Daria <shkrobadaria@mail.ru>
2025-06-26 10:55:45 +0300

    check 4-4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 import store from './slices/index.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
 // eslint-disable-next-line no-unused-vars
 import i18next from './18n.js'
 import Socket from './components/Socket.jsx'
+import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
